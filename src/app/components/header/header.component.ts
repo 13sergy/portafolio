d44b750f8e1cfedae7506b3d4852f5af5a9f2a30
Sergio Gómez <sergy_13-36@hotmail.com>
2017-11-25 20:48:39 +0100

    Servicio Info creado, header y footer.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
+import {InformacionService} from "../../services/informacion.service";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  constructor(public _is: InformacionService) { /*_is es la abreviatura de informacion service*/
 
+  }
 
 }
