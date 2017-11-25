44e153fa68032fb7a0597495352e492a3c6e737b
Sergio Gómez <sergy_13-36@hotmail.com>
2017-11-25 21:34:13 +0100

    Servicio de Firebase para seccion Sobre nosotros

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -5,14 +5,29 @@ import {Http} from "@angular/http";
 export class InformacionService {
 
   info: any = {};
-  cargada:boolean = false;
+  equipo: any[] = [];
+  cargada: boolean = false;
+  cargada_sobre_nosotros: boolean = false;
 
   constructor(public http: Http) {
+    this.carga_info();
+    this.carga_sobre_nosotros();
+  }
+
+  public carga_info() {
     this.http.get('assets/data/info.pagina.json')
       .subscribe(data => {
-        console.log(data.json());
         this.info = data.json();
         this.cargada = true;
+      });
+  }
+
+  public carga_sobre_nosotros() {
+    this.http.get('https://portafolio-d76f7.firebaseio.com/equipo.json')
+      .subscribe(data => {
+        // console.log(data.json());
+        this.equipo = data.json();
+        this.cargada_sobre_nosotros = true;
       });
   }
 
