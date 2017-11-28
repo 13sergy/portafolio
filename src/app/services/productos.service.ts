b4b07c3fd469d83d4b465a6a298cb7c9cb1b5128
Sergio Gómez <sergy_13-36@hotmail.com>
2017-11-28 20:48:06 +0100

    Pagina de producto y ruta lista.

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,12 +1,11 @@
 import {Injectable} from "@angular/core";
 import {Http} from "@angular/http";
-import {} from "@angular/log";
 
 
 @Injectable()
 export class ProductosService {
 
-  prodctos: any[] = [];
+  productos: any[] = [];
   cargando_productos: boolean = true;
 
   constructor(private http: Http) {
@@ -18,9 +17,11 @@ export class ProductosService {
     this.cargando_productos = true;
     this.http.get('https://portafolio-d76f7.firebaseio.com/productos_idx.json')
       .subscribe(data => {
-        console.log(data.json());
-        this.productos = data.json();
-        this.cargando_productos = false;
+        // console.log(data.json());
+        setTimeout(() => {
+          this.productos = data.json();
+          this.cargando_productos = false;
+        }, 1500);
       });
   }
 
